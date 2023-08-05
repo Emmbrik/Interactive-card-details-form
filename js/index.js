cbc37803cbb51d3859a413e926820749e7dcff99
Emmbrik <hemmyemmix@gmail.com>
2023-08-05 05:41:25 +0100

    Added live typing on the card Fixed some issues with validations

--- js/index.js
+++ js/index.js
@@ -5,23 +5,60 @@ let cardNumber = document.getElementById("number");
 let expMonth = document.getElementById("month");
 let expYear = document.getElementById("year")
 let cvv = document.getElementById("cvv");
+let cardContent = document.querySelectorAll(".card-content");
 let date = document.querySelector(".date");
-let submit = document.getElementById("submit");
-let completeState = document.querySelector("complete-state");
-let form = document.querySelector("form");
+let submitButton = document.getElementById("submit");
+let continueButton = document.getElementById("continue");
+let completeState = document.querySelector(".complete-state");
+let form = document.querySelector(".form");
 
 inputField.forEach((input, index) => {
-    input.addEventListener("keypress", function(){
+    let previousValue = "";
 
+    input.addEventListener("keydown", function(event) {
+          if (event.key === "Backspace") {
+            // Handle backspace key press
+            input.value = input.value.slice(0, -1);
+            event.preventDefault(); // Prevent the default backspace behavior (navigating back)
+            writeOnCard(input);
+          }
+        });
+
+    input.addEventListener("keyup", function(event) {
+        if (event.key === "Backspace" && input.value.trim() === "") {
+            // Restore the previous value if input is blank after backspace release
+            input.value = previousValue;
+            writeOnCard(input);
+        }
+        });
+
+    input.addEventListener("input", function(event){
+        // Handles all inputs with the key press
         if(input.value.trim() === ""){
             blankErrorMessages[index].classList.add("visible");
         }
         else{
             blankErrorMessages[index].classList.remove("visible");
+            writeOnCard(input);
         }
     });
 });
 
+
+function writeOnCard(input){
+    if(input === cardName){
+        document.querySelector(".card-name").innerHTML = input.value;
+    }
+    else if(input === cardNumber){
+        document.querySelector(".card-num").innerHTML = input.value;
+    }
+    else if(input === cvv){
+        document.querySelector(".card-cvv").innerHTML = input.value;
+    }
+    else{
+        console.log("its works");
+    }
+}
 
 function validateExpiryDate(dateString) {
     const regex = /^(0[1-9]|1[0-2])\/?([0-9]{2})$/;
@@ -36,69 +73,46 @@ function validateExpiryDate(dateString) {
 
 
 function validInputs(){
-    let parentClass;
-    let error;
     let status = true;
 
-    [cardName, cardNumber, expMonth, expYear].forEach(input =>{
-        
-        if(input.value === ""){
-            parentClass = input.parentNode;
-            error= parentClass.querySelector(".blank-errorMessage");
-            error.classList.add("visible");
-            console.log("hello");
+
+    inputField.forEach((input, index) => {
+        if(input.value.trim() === ""){
+            blankErrorMessages[index].classList.add("visible");
             status = false;
         }
-    });
+    })
 
-    if(!validateExpiryDate("07/23")){
-        date.forEach(input => {
-            input.classList.add("visible");
-        })
+
+    if(!validateExpiryDate("07/i3")){
+        date.querySelectorAll(".blank-errorMessage").forEach(message =>{
+            message.classList.add("visible");
+        }
+            )
+        // date.forEach(input => {
+        //     input.classList.add("visible");
+        // })
         status = false;
     }
     
     return status;
-
-    // if(cardName.value === ""){
-    //     parentClass = cardName.parentNode;
-    //     error= parentClass.querySelector(".blank-errorMessage");
-    //     error.classList.add("visible");
-    //     status = false;
-    // }
-    // else if(cardNumber.value === ""){
-    //     parentClass = cardNumber.parentNode;
-    //     error= parentClass.querySelector(".blank-errorMessage");
-    //     error.classList.add("visible");
-    //     status = false;
-    // }
-    // else if(expMonth.value === ""){
-    //     parentClass = expMonth.parentNode;
-    //     error= parentClass.querySelector(".blank-errorMessage");
-    //     error.classList.add("visible");
-    //     status = false;
-    // }
-    // else if(expYear.value === ""){
-    //     parentClass = expYear.parentNode;
-    //     error= parentClass.querySelector(".blank-errorMessage");
-    //     error.classList.add("visible");
-    //     status = false;
-    // }
-    // else if(cvv.value === ""){
-    //     parentClass = cvv.parentNode;
-    //     error= parentClass.querySelector(".blank-errorMessage");
-    //     error.classList.add("visible");
-    //     status = false;
-    // }
+ 
 
 }
 
 
 submit.addEventListener("click", function(){
     if(validInputs()){
-        console.log("hello");
-        form.classList.add(".hide");
-        completeState.remove(".hide");
+        form.classList.add("hide");
+        completeState.classList.remove("hide");
+    }
+
+})
+
+continueButton.addEventListener("click", function(){
+    if(validInputs()){
+        form.classList.remove("hide");
+        completeState.classList.add("hide");
     }
 
 })
